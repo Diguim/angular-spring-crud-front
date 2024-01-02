a5d9ffd128702396552d36ce5709aca0c93078b5
Diogo Otoni <diogootoni@hotmail.com>
2024-01-02 15:40:14 -0300

    update: standalone components option 3

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,31 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { provideRouter, Routes } from '@angular/router';
+import { CoursesRoutingModule } from './app/courses/courses-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'courses' },
+    {
+        path: 'courses',
+        loadChildren: () => import("./app/courses/courses.module").then(m => m.CoursesModule)
+    }
+];
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, MatToolbarModule, CoursesRoutingModule),
+        provideAnimations(),
+        provideRouter(routes),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
